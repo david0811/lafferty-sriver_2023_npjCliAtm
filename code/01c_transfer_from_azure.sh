98614721af1110b07d7d5d955249e76437565947
David Lafferty <davidcl2@illinois.edu>
2023-01-18 13:19:41 -0500

    regional uc

--- code/01c_transfer_from_azure.sh
+++ code/01c_transfer_from_azure.sh
@@ -3,11 +3,16 @@
 # make sure executable is in path
 export PATH=$PATH:/storage/home/dcl5300/work/azcopy_linux_amd64_10.16.1
 
-OUT_PATH="/gpfs/group/kaf26/default/dcl5300/lafferty-sriver_inprep_tbh_DATA/quantiles/"
+# OUT_PATH="/gpfs/group/kaf26/default/dcl5300/lafferty-sriver_inprep_tbh_DATA/obs_data/quantiles/"
 
-azcopy copy 'https://mpctransfer.blob.core.windows.net/mpctransfer/quantiles/era5_precip_quantiles_isimip' "${OUT_PATH}" --recursive
-azcopy copy 'https://mpctransfer.blob.core.windows.net/mpctransfer/quantiles/era5_precip_quantiles_gardsv' "${OUT_PATH}" --recursive
-azcopy copy 'https://mpctransfer.blob.core.windows.net/mpctransfer/quantiles/era5_precip_quantiles_nex-cil-deepsd' "${OUT_PATH}" --recursive
-azcopy copy 'https://mpctransfer.blob.core.windows.net/mpctransfer/quantiles/era5_temperature_quantiles_isimip' "${OUT_PATH}" --recursive
-azcopy copy 'https://mpctransfer.blob.core.windows.net/mpctransfer/quantiles/era5_temperature_quantiles_gardsv' "${OUT_PATH}" --recursive
-azcopy copy 'https://mpctransfer.blob.core.windows.net/mpctransfer/quantiles/era5_temperature_quantiles_nex-cil-deepsd' "${OUT_PATH}" --recursive
+# azcopy copy 'https://mpctransfer.blob.core.windows.net/mpctransfer/quantiles/era5_precip_quantiles_isimip' "${OUT_PATH}" --recursive
+# azcopy copy 'https://mpctransfer.blob.core.windows.net/mpctransfer/quantiles/era5_precip_quantiles_gardsv' "${OUT_PATH}" --recursive
+# azcopy copy 'https://mpctransfer.blob.core.windows.net/mpctransfer/quantiles/era5_precip_quantiles_nex-cil-deepsd' "${OUT_PATH}" --recursive
+# azcopy copy 'https://mpctransfer.blob.core.windows.net/mpctransfer/quantiles/era5_temperature_quantiles_isimip' "${OUT_PATH}" --recursive
+# azcopy copy 'https://mpctransfer.blob.core.windows.net/mpctransfer/quantiles/era5_temperature_quantiles_gardsv' "${OUT_PATH}" --recursive
+# azcopy copy 'https://mpctransfer.blob.core.windows.net/mpctransfer/quantiles/era5_temperature_quantiles_nex-cil-deepsd' "${OUT_PATH}" --recursive
+
+
+OUT_PATH="/gpfs/group/kaf26/default/dcl5300/lafferty-sriver_inprep_tbh_DATA/obs_data/annual/era5/"
+
+azcopy copy 'https://mpctransfer.blob.core.windows.net/mpctransfer/era5/annual/' "${OUT_PATH}" --recursive
